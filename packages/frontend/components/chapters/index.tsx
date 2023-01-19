492890c7c15d0cd3d094ab906ab07abbe75eecc5
Falco Rodenburg <falcorodenburg@gmail.com>
2023-01-19 17:42:21 +0700

    initial prison story setup

--- packages/frontend/components/chapters/index.tsx
+++ packages/frontend/components/chapters/index.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import CurrentChapter from "./chapter";
 import Chapter from "../../types/Chapter";
-import ChaptersJSON from "../../data/chapters.json";
+import ChaptersJSON from "../../data/test.json";
 
 const Chapters = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
