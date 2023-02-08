57f9804599dc91b9db3126302ba800cc609c04cc
samueloh99 <samuel_oh@hotmail.com>
2023-02-08 20:05:40 -0300

    Modified reputation page

--- packages/frontend/components/Reputation/RepValue.tsx
+++ packages/frontend/components/Reputation/RepValue.tsx
@@ -1,35 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
+import parse from "html-react-parser";
 
 interface Props {
   reputation: number;
 
-  stats: Array<{
+  registers: Array<{
     name: string;
+    bits: number;
     color: string;
-    value: number;
+    posRep: number;
+    negRep: number;
+    minValue: number;
   }>;
 }
 
-const RepValue: React.FC<Props> = ({ reputation, stats }) => {
+const RepValue: React.FC<Props> = ({ reputation, registers }) => {
   const colorReputation = (reputationNum: number) => {
-    let reputation = reputationNum.toString(stats.length * 2).padStart(stats.length * 2, "0");
+    let reputation = reputationNum
+      .toString(registers.length * 2)
+      .padStart(registers.length * 2, "0");
+
     let colorReputation = "";
     let currentStat = 0;
     for (let i = 0; i < reputation.length; i++) {
       if (i % 2 === 0 && i !== 0) {
         currentStat++;
       }
-      colorReputation += `<span class="${stats[currentStat].color}">${reputation[i]}</span>`;
+      colorReputation += `<span className="${registers[currentStat].color}">${reputation[i]}</span>`;
     }
     return colorReputation;
   };
 
-  // TODO: Find a better way of doing this that doesnt' involve dangerouslySetInnerHTML
+  // TODO: Find a better way of doing this that doesnt' involve dangerouslySetInnerHTML - CHECK
   return (
-    <div
-      className="text-4xl"
-      dangerouslySetInnerHTML={{ __html: "<span style='color: #000000;'>0x</span>" + colorReputation(reputation) }}
-    />
+    <div className="text-4xl">
+      <span style={{ color: "#000000" }}>0x</span>
+      {parse(colorReputation(reputation))}
+    </div>
   );
 };
 
--- packages/frontend/pages/reputation.tsx
+++ packages/frontend/pages/reputation.tsx
@@ -1,11 +1,10 @@
-import Link from "next/link";
 import Layout from "../components/Layout";
 import Player from "../components/Reputation/Player";
 
 const ReputationPage = () => (
-    <Layout title="TAZ v2">
-        <Player />
-    </Layout>
+  <Layout title="TAZ v2">
+    <Player />
+  </Layout>
 );
 
 export default ReputationPage;
